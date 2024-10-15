ef923608574ebcab3ef2828933fde1d875cbb0bc
Antonino <ant.iaria@stud.uniroma3.it>
2024-10-15 15:53:00 +0200

    minor changes for benchmaerk purpose only

--- Sincero/server/api/counter.ts
+++ Sincero/server/api/counter.ts
@@ -1,12 +1,35 @@
 import { defineEventHandler } from 'h3';
+import { promises as fs } from 'fs';
+const noncesPath = "../Sincero/server/api/benchmarks/loadingTimes.json"; 
+
 export default defineEventHandler(async (event) => {
-    // TODO SAVE TIMES
-    // let counter = getCookie(event, '__session') || 0
+    // Read the request body
+    const body = await readBody(event);
+    console.log("this is the passed body of counter", body);
+
+    const timestamp = body.timestamp;
+    const apiLoadTime = body.apiLoadTime;
+    const totalLoadTime = body.totalLoadTime;
+
+    const newEntry = { timestamp, apiLoadTime, totalLoadTime };
+
+    // Read the existing data from the JSON file
+    let dataArray = [];
+    try {
+        const fileContents = await fs.readFile(noncesPath, 'utf-8');
+        dataArray = fileContents ? JSON.parse(fileContents).data : [];
+    } catch (error) {
+        console.error("Error reading file:", error);
+    }
+
+    // Add the new entry to the data array
+    dataArray.push(newEntry);
+
+    // Write the updated data back to the JSON file
+    const updatedData = { data: dataArray };
+    await fs.writeFile(noncesPath, JSON.stringify(updatedData, null, 2));
   
-    // Increase counter cookie by 1
-    // setCookie(event, 'counter', ++counter)
-  
-    // Send JSON response
-    // return { counter }
-  })
-  
+    return{
+      code: 200
+    }
+});
